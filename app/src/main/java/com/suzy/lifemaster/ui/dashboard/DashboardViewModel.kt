43c9cd53d9c5d923fb2602476426661970452316
suzy-mo <83625098+Suzy-mo@users.noreply.github.com>
2024-03-08 18:39:06 +0800

    init commit

---
+++ app/src/main/java/com/suzy/lifemaster/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.suzy.lifemaster.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
